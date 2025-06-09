4044282364ef071f1c8998a3638f4d67b5ebd81a
santiagodev <sanxp18@gmail.com>
2025-06-08 22:25:30 -0400

    add a little refactor to the logic for the addition of background color and emojis of the container of each project

--- src/index.js
+++ src/index.js
@@ -3,31 +3,48 @@ const projects = document.querySelector('.projects');
 
 const loadLinks = () => {
   const linkList = [
-    { name: 'Awesome Movies', url: 'https://santiagodev10.github.io/Awesome-Movies/' },
-    { name: 'Product List', url: 'https://product-list-h48p21za3-santiago-s-projects-57b87035.vercel.app/' },
-    { name: 'Advice Generator App', url: 'https://santiagodev10.github.io/advice-generator-app/' },
-    { name: 'Article Preview Component', url: 'https://santiagodev10.github.io/Article-preview-component/' },
+    { 
+      name: 'Awesome Movies', 
+      url: 'https://santiagodev10.github.io/Awesome-Movies/',
+      color: 'project1',
+      emoji: '🎬' 
+    },
+    { 
+      name: 'Product List',
+      url: 'https://product-list-h48p21za3-santiago-s-projects-57b87035.vercel.app/',
+      color: 'project2',
+      emoji: '🍕'
+    },
+    { 
+      name: 'Advice Generator App', 
+      url: 'https://santiagodev10.github.io/advice-generator-app/',
+      color: 'project3',
+      emoji: '💡' 
+    },
+    { 
+      name: 'Article Preview Component', 
+      url: 'https://santiagodev10.github.io/Article-preview-component/',
+      color: 'project4',
+      emoji: '📄' 
+    },
   ];
-
-const projectClasses = ['project1', 'project2', 'project3', 'project4'];
-const emojis = ['📽️', '🍰', '⚙️', '⭐'];
-let classIndex = 0;
 
   linkList.forEach(link => {
     const container = document.createElement('div');
     const anchor = document.createElement('a');
     container.classList.add('project');
-    container.classList.add(projectClasses[classIndex]); // Asigna la clase del array
+    container.classList.add(link.color);
+    container.addEventListener('click', () => {
+      window.open(link.url, '_blank');
+    });
 
     anchor.href = link.url;
     anchor.classList.add('project-link');
-    anchor.textContent = `${link.name} ${emojis[classIndex]}`; // Añade emoji al texto
+    anchor.textContent = `${link.name} ${link.emoji}`;
     anchor.target = '_blank';
     
     container.appendChild(anchor);
     projects.appendChild(container);
-
-    classIndex = (classIndex + 1) % projectClasses.length; // Avanza al siguiente índice, reinicia si llega al final
   });
 };
 
